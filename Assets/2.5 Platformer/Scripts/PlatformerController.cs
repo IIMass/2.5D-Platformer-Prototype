327bf762e7167c95c5e14fbba9c91e9f923dc596
Massitao <pablo.gomezplaton@gmail.com>
2021-10-24 19:19:08 +0200

    Character mesh and animations added

---
+++ Assets/2.5 Platformer/Scripts/PlatformerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+    [SerializeField] private float maxDownForce;
+
+    private Vector3 moveDir = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveController();
+    }
+
+    private void MoveController()
+    {
+        if (controller.isGrounded)
+        {
+            moveDir.z = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        }
+
+        moveDir.y = Mathf.Clamp(moveDir.y - (gravity * Time.deltaTime), -maxDownForce, float.MaxValue);
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            moveDir.y = jumpForce;
+            animator.SetTrigger("Jump");
+        }
+
+        controller.Move(moveDir * Time.deltaTime);
+
+        if (moveDir.z > 0f)
+        {
+            animator.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (moveDir.z < 0f)
+        {
+            animator.transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+        }
+
+
+        animator.SetFloat("Speed", controller.velocity.magnitude);
+        animator.SetBool("Grounded", controller.isGrounded);
+    }
+}
