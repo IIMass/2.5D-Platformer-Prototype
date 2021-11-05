50960e652ba673257c0353af720d635c1ccb1ffd
Massitao <pablo.gomezplaton@gmail.com>
2021-11-05 22:29:50 +0100

    Added Ledge Grab and Climb

---
+++ Assets/2.5 Platformer/Scripts/LedgeChecker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LedgeChecker : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LedgeGrabChecker"))
+        {
+            other.GetComponentInParent<PlatformerController>().LedgeGrab();
+        }
+    }
+}
--- Assets/2.5 Platformer/Scripts/PlatformerController.cs
+++ Assets/2.5 Platformer/Scripts/PlatformerController.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 
 public class PlatformerController : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private CharacterController controller;
     [SerializeField] private Animator animator;
+    [SerializeField] private Transform ledgeChecker;
 
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravity;
     [SerializeField] private float maxDownForce;
+
+    [Header("States")]
+    [SerializeField] private bool onLedge;
+    [SerializeField] private bool climbing;
+
+    [Header("Ledge Climb")]
+    [SerializeField] private Vector2 climbOffset;
 
     private Vector3 moveDir = Vector3.zero;
 
@@ -19,10 +29,13 @@ public class PlatformerController : MonoBehaviour
     void Update()
     {
         MoveController();
+        LedgeStartClimb();
     }
 
     private void MoveController()
     {
+        if (onLedge) return;
+
         if (controller.isGrounded)
         {
             moveDir.z = Input.GetAxisRaw("Horizontal") * moveSpeed;
@@ -50,5 +63,41 @@ public class PlatformerController : MonoBehaviour
 
         animator.SetFloat("Speed", controller.velocity.magnitude);
         animator.SetBool("Grounded", controller.isGrounded);
+    }
+
+
+    private void LedgeStartClimb()
+    {
+        // If the player presses Space, and is on a ledge and not currently climbing it...
+        if (Input.GetKeyDown(KeyCode.Space) && onLedge && !climbing)
+        {
+            animator.SetTrigger("Climb");   // Trigger the Climb animation
+            climbing = true;                // This state avoids triggering again the Climb animation
+        }
+    }
+
+    public void LedgeGrab()
+    { 
+        onLedge = true;                     // This state disables the character movement
+        moveDir = Vector3.zero;             // Resets the controller's momentum
+        animator.SetTrigger("OnLedge");     // Triggers the Ledge Grab animation
+        animator.SetFloat("Speed", 0f);     // Resets the movement speed animator value
+
+        // Sets the position of the character controller near the ledge
+        transform.position = new Vector3(transform.position.x, transform.position.y, ledgeChecker.position.z);
+    }
+
+
+    public void LedgeClimb()
+    {
+        // Moves the character controller to an approximate position.
+        // Z offset is applied differently taking into account the direction the controller is facing
+        transform.position = new Vector3
+            (transform.position.x,
+            transform.position.y + climbOffset.y,
+            transform.position.z + (Mathf.Sign(animator.transform.eulerAngles.y) * climbOffset.x));
+
+        onLedge = false;
+        climbing = false;
     }
 }
