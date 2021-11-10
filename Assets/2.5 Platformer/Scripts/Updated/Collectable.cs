134c342c54894fde4e3e5e4cd7b0f691fde11433
Massitao <pablo.gomezplaton@gmail.com>
2021-11-10 23:37:30 +0100

    Collectable added

---
+++ Assets/2.5 Platformer/Scripts/Updated/Collectable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [Header("Collectable Variables")]
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float moveSinAmplitude;
+    [SerializeField] private float moveSinFrequency;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = startPos + Vector3.up * Mathf.Sin(Time.time * moveSinFrequency) * moveSinAmplitude;
+        transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlatformerController controller))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
